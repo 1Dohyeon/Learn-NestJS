3143bb3da608d713c0176d3211aed8e13d5be17b
1Dohyeon <dh1072005@gmail.com>
2023-12-21 22:44:01 +0900

    exress 에서의 get과 post 기초 공부

--- letsStart/dist/app.js
+++ letsStart/dist/app.js
@@ -1,3 +1,18 @@
 "use strict";
-console.log("hello world!!");
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+var port = 8000;
+app.get('/', function (req, res) {
+    res.send({ name: "dohyeon", age: 22, });
+});
+app.get('/test', function (req, res) {
+    res.send({ name: "dohyeon", age: 22, });
+});
+app.post('/test', function (req, res) {
+    res.send({ city: "Seoul" });
+});
+app.listen(port, function () {
+    console.log("Example app listening on port http://localhost:" + port);
+});
 //# sourceMappingURL=app.js.map
