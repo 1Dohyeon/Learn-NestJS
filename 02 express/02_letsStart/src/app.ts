107ba21aca5485d07d40e6eace142164c04078a0
1Dohyeon <dh1072005@gmail.com>
2024-01-06 13:08:53 +0900

    공부 세팅
    01_letsStart 내용 02_letsStart에 덮어 씌움

--- 02 express/02_letsStart/src/app.ts
+++ 02 express/02_letsStart/src/app.ts
@@ -1,1 +1,8 @@
-console.log("hello world!!");
+// 타입스크립트를 노드 환경에서 사용할 때
+// npm run start:dev <--- 코드가 바뀔 때마다 실시간 업데이트
+
+import * as express from "express";
+
+const app: express.Express = express();
+const port: number = 8000;
+
