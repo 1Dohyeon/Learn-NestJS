cf8f7c8feedcf9c0746c9d911fb700012b0dccfe
1Dohyeon <dh1072005@gmail.com>
2024-01-06 11:09:54 +0900

    파일 위치 변경

--- 02 express/letsStart/dist/app.js
+++ 02 express/letsStart/dist/app.js
@@ -1,18 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var app_model_1 = require("./app.model");
 var app = express();
 var port = 8000;
-app.get('/', function (req, res) {
-    res.send({ name: "dohyeon", age: 22, });
-});
-app.get('/test', function (req, res) {
-    res.send({ name: "dohyeon", age: 22, });
-});
-app.post('/test', function (req, res) {
-    res.send({ city: "Seoul" });
-});
 app.listen(port, function () {
     console.log("Example app listening on port http://localhost:" + port);
+});
+app.get('/', function (req, res) {
+    console.log(req);
+    res.send({ Cat: app_model_1.Cat });
 });
 //# sourceMappingURL=app.js.map
---
+++ 02 express/letsStart/src/app.ts
@@ -0,0 +1,30 @@
+// 타입스크립트를 노드 환경에서 사용할 때
+// npm run start:dev <--- 코드가 바뀔 때마다 실시간 업데이트
+
+import * as express from "express";
+import { Cat } from './app.model';
+
+const app: express.Express = express();
+const port: number = 8000;
+
+// app.get('/', (req: express.Request, res: express.Response) => {
+//   res.send({name: "dohyeon", age: 22,});
+// });
+
+// app.get('/test', (req: express.Request, res: express.Response) => {
+//     res.send({name: "dohyeon", age: 22,});
+// });
+
+// app.post('/test', (req: express.Request, res: express.Response) => {
+//     res.send({city: "Seoul"});
+// });
+
+// 서버를 엶
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send({ Cat });
+});
