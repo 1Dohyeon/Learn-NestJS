b91f804273864508966a8c2ad875dc63afb43fc9
1Dohyeon <dh1072005@gmail.com>
2024-01-09 12:18:36 +0900

    Day4
    nest g co 명령어로 컨트롤러 생성

--- 03_practice-nest/src/cats/cats.module.ts
+++ 03_practice-nest/src/cats/cats.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CatsController } from './cats.controller';
 
 // nest g mo cats 로 자동으로 모듈 생성
 
-@Module({})
+@Module({
+  controllers: [CatsController],
+  providers: [],
+})
 export class CatsModule {}
