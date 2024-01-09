48e61299990acfdfbc49c89bcb5dd0b451dc1eca
1Dohyeon <dh1072005@gmail.com>
2024-01-09 12:43:05 +0900

    Day4
    controller 임시 작성

--- 03_practice-nest/src/cats/cats.controller.ts
+++ 03_practice-nest/src/cats/cats.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  // cats/
+  @Get()
+  getAllCat() {
+    return 'all cat';
+  }
+
+  // cats/:id
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete cat';
+  }
+}
