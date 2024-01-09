68a75f83c893025b11997c7b9cd1c9e7705e3d3d
1Dohyeon <dh1072005@gmail.com>
2024-01-09 12:19:35 +0900

    Day4
    nest g service 를 이용하여 서비스 파일 생성

--- 03_practice-nest/src/cats/cats.module.ts
+++ 03_practice-nest/src/cats/cats.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
+import { CatsService } from './cats.service';
 
 // nest g mo cats 로 자동으로 모듈 생성
 
 @Module({
   controllers: [CatsController],
-  providers: [],
+  providers: [CatsService],
 })
 export class CatsModule {}
