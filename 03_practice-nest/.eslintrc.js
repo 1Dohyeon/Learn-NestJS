4352fb866cc41ebe3f04721920203b7b3b6f4cee
1Dohyeon <dh1072005@gmail.com>
2024-01-09 20:49:59 +0900

    Day4
    middleware logger 생성

--- 03_practice-nest/.eslintrc.js
+++ 03_practice-nest/.eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
           endOfLine: 'auto',
       },
     ],
-    'no-console': 'warn',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
