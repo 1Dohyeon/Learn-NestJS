0e0bd119981abc8f7b92c4a5607c396b7ba31b39
1Dohyeon <dh1072005@gmail.com>
2024-01-09 20:54:45 +0900

    Day4
    logger 학습

--- 03_practice-nest/src/logger/logger.middleware.ts
+++ 03_practice-nest/src/logger/logger.middleware.ts
@@ -1,10 +1,12 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(req.ip);
+    this.logger.log(req.ip, req.originalUrl);
     next();
   }
 }
