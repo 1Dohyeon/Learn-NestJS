4cf2d3832d7a4fa43ae8aa9aa54729ac1c0087cb
1Dohyeon <dh1072005@gmail.com>
2024-01-30 23:48:17 +0900

    Day11
    이벤트 설계와 기본 서비스 로직 완성

--- 05_project2/public/js/scripts.js
+++ 05_project2/public/js/scripts.js
@@ -10,9 +10,34 @@ const formElement = getElementById('chat_form');
 const drawHelloStranger = (username) =>
   (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
 
+const drawNewChat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `<div>${message}<div>`;
+
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
+};
+
+// event callback functions
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const inputValue = e.target.elements[0].value;
+  if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    // 화면에다가 그리기
+    drawNewChat(inputValue);
+    e.target.elements[0].value = '';
+  }
+};
+
 // grobal socket handler
 socket.on('user_connected', (username) => {
-  console.log(`${username} connected!`);
+  drawNewChat(`${username} connected!`);
+});
+
+socket.on('new_chat', (data) => {
+  const { chat, username } = data;
+  drawNewChat(`${username}: ${chat}`);
 });
 
 function helloUser() {
@@ -24,6 +49,8 @@ function helloUser() {
 
 function init() {
   helloUser();
+  // 이벤트 연결
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
