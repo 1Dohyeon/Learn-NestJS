daa919b9d68b86bad3d0421c269cc187380f4fbf
1Dohyeon <dh1072005@gmail.com>
2024-01-30 22:35:40 +0900

    Day11
    emit, on, broadcast 공부

--- 05_project2/public/js/scripts.js
+++ 05_project2/public/js/scripts.js
@@ -1,4 +1,4 @@
-const socket = io('/');
+const socket = io('/chattings');
 
 const getElementById = (id) => document.getElementById(id) || null;
 
@@ -6,8 +6,20 @@ const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
+// draw functions
+const drawHelloStranger = (username) =>
+  (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+
+// grobal socket handler
+socket.on('user_connected', (username) => {
+  console.log(`${username} connected!`);
+});
+
 function helloUser() {
   const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => {
+    drawHelloStranger(data);
+  });
 }
 
 function init() {
--- 05_project2/src/chats/chats.gateway.ts
+++ 05_project2/src/chats/chats.gateway.ts
@@ -1,9 +1,45 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconnected : ${socket.id} ${socket.nsp}`);
+  }
+
+  // 커넥션이 되자마자 작동
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected : ${socket.id} ${socket.nsp}`);
+  }
+
+  // 시작하자마자 생성자 다음으로 실행
+  afterInit() {
+    this.logger.log('init');
+  }
+
+  @SubscribeMessage('new_user')
+  handleNewUser(
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    socket.broadcast.emit('user_connected', username);
+    return username;
   }
 }
