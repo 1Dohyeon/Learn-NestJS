cce004d2a1167a71cb56079291603d1b809ca32d
1Dohyeon <dh1072005@gmail.com>
2024-02-02 11:41:22 +0900

    Day13
    mysql setup

--- 06_typeorm/README.md
+++ 06_typeorm/README.md
@@ -71,3 +71,10 @@ Nest is an MIT-licensed open source project. It can grow thanks to the sponsors 
 ## License
 
 Nest is [MIT licensed](LICENSE).
+
+## Setup
+
+```
+npm i --save @nestjs/typeorm typeorm mysql2
+npm i --save @nestjs/config
+```
--- 06_typeorm/src/app.module.ts
+++ 06_typeorm/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: 3306,
+      username: 'admin',
+      password: process.env.MYSQL_PW,
+      database: 'nest',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
